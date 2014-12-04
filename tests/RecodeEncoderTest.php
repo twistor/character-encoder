b26fe034e2a0900018cf8500dad10315d4dfac05
Chris Leppanen <chris.leppanen@gmail.com>
2014-12-03 18:33:06 -0800

    Conditionally declare recode tests.

--- tests/RecodeEncoderTest.php
+++ tests/RecodeEncoderTest.php
@@ -9,11 +9,16 @@ namespace CharacterEncoder\Tests;
 
 use CharacterEncoder\RecodeEncoder;
 
+// Have to figure out how to get recode installed on travis.
+if (extension_loaded('recode')) {
+
 /**
  * @covers \CharacterEncoder\RecodeEncoder
  */
 class RecodeEncoderTest extends EncoderTest {
 
   protected $encoderClass = 'CharacterEncoder\RecodeEncoder';
+
+}
 
 }
